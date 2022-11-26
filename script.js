4211e923370b9e891f5c7843a9e8167e2e9d5437
Edward <ecampbell3015@gmail.com>
2022-11-25 17:18:15 -0800

    finish services page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const homeFooterQuoteBtn = document.querySelector('.home__footer-quote-btn');
-const homeFooterModalOpenBlur = document.querySelector('.home__footer-modal-open-wrap');
-const homeFooterForm = document.querySelector('.home__footer-quote-form');
+const mainFooterQuoteBtn = document.querySelector('.main__footer-quote-btn');
+const mainFooterModalOpenBlur = document.querySelector('.main__footer-modal-open-wrap');
+const mainFooterForm = document.querySelector('.main__footer-quote-form');
 let quoteFormIsOpen = false;
 
 
@@ -9,12 +9,12 @@ let quoteFormIsOpen = false;
         const isQuoteForm = e.target.matches("[data-quote-form]");
         const isQuoteFormOpenBtn = e.target.matches("[data-quote-form-open]");
         if(isQuoteFormOpenBtn) {
-            homeFooterModalOpenBlur.style.display = 'block';
-            homeFooterForm.style.display = "flex";
+            mainFooterModalOpenBlur.style.display = 'block';
+            mainFooterForm.style.display = "flex";
             quoteFormIsOpen = true;
         } else if (quoteFormIsOpen && !isQuoteForm) {
-            homeFooterModalOpenBlur.style.display = 'none';
-            homeFooterForm.style.display = "none";
+            mainFooterModalOpenBlur.style.display = 'none';
+            mainFooterForm.style.display = "none";
             quoteFormIsOpen = false;
         }
     });
