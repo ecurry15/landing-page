2f4ca044a374a87ffdefb7c55ca3994deff78a2f
Edward <ecampbell3015@gmail.com>
2022-11-27 19:28:59 -0800

    make mobile sidebar functional and responsive

--- script.js
+++ script.js
@@ -2,12 +2,49 @@ const mainFooterQuoteBtn = document.querySelector('.main__footer-quote-btn');
 const mainFooterModalOpenBlur = document.querySelector('.main__footer-modal-open-wrap');
 const mainFooterForm = document.querySelector('.main__footer-quote-form');
 let quoteFormIsOpen = false;
+const mobileSideBarOpenIcon = document.querySelector('.main-nav__mobile-menu-icon');
+const mobileSideBar = document.querySelector('.mobile__nav-sideBar');
+const mobileSideBarCloseIcon = document.querySelector('.mobile__sideBar-close-btn');
+const mobileSideBarNav = document.querySelector('.mobile-sideBar__nav');
+const mobileSideBarBtn = document.querySelector('.mobile-sideBar__btn');
+const mobileSideBarLogo = document.querySelector('.mobile-sideBar__logo');
 
+mobileSideBarOpenIcon.addEventListener('pointerdown', () => {
+let largeMobile = window.matchMedia('(min-width: 768px)');
+if(largeMobile.matches) {
+mobileSideBar.style.animation = "large-mobile-sideBar-open .5s forwards";
+} else if (!largeMobile.matches) {
+    mobileSideBar.style.animation = "small-mobile-sideBar-open .5s forwards";
+}
+mainFooterModalOpenBlur.style.display = 'block';
+setTimeout(() => {
+    mobileSideBarCloseIcon.style.display = "block";
+    mobileSideBarNav.style.display = "flex";
+    mobileSideBarBtn.style.display = "block";
+    mobileSideBarLogo.style.display = "block";
+  }, "300")
+});
+
+mobileSideBarCloseIcon.addEventListener('pointerdown', () => {
+    let largeMobile = window.matchMedia('(min-width: 768px)');
+if(largeMobile.matches) {
+mobileSideBar.style.animation = "large-mobile-sideBar-close .5s forwards";
+} else if (!largeMobile.matches) {
+    mobileSideBar.style.animation = "small-mobile-sideBar-close .5s forwards";
+}
+mobileSideBarCloseIcon.style.display = "none";
+    mobileSideBarNav.style.display = "none";
+    mobileSideBarBtn.style.display = "none";
+    mobileSideBarLogo.style.display = "none";
+    mainFooterModalOpenBlur.style.display = 'none';
+});
 
 
  document.addEventListener('click', (e) => {
         const isQuoteForm = e.target.matches("[data-quote-form]");
         const isQuoteFormOpenBtn = e.target.matches("[data-quote-form-open]");
+        const isSideBarQuoteFormOpenBtn = e.target.matches("[data-sideBar-quote-form-open]");
+        let largeMobile = window.matchMedia('(min-width: 768px)');
         if(isQuoteFormOpenBtn) {
             mainFooterModalOpenBlur.style.display = 'block';
             mainFooterForm.style.display = "flex";
@@ -16,6 +53,18 @@ let quoteFormIsOpen = false;
             mainFooterModalOpenBlur.style.display = 'none';
             mainFooterForm.style.display = "none";
             quoteFormIsOpen = false;
+        } else if (isSideBarQuoteFormOpenBtn) {
+            mainFooterModalOpenBlur.style.display = 'block';
+            mainFooterForm.style.display = "flex";
+            quoteFormIsOpen = true;
+            if(largeMobile.matches) {
+                mobileSideBar.style.animation = "large-mobile-sideBar-close .5s forwards";
+                } else if (!largeMobile.matches) {
+                    mobileSideBar.style.animation = "small-mobile-sideBar-close .5s forwards";
+                }
+                mobileSideBarCloseIcon.style.display = "none";
+                    mobileSideBarNav.style.display = "none";
+                    mobileSideBarBtn.style.display = "none";
+                    mobileSideBarLogo.style.display = "none";
         }
     });
-
